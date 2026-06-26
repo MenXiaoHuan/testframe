eadda1cf5d780bf1c6d3d6c1da2556f55d146746
bytedance <xuzihao.404@bytedance.com>
2026-06-26 12:26:52 +0800

    chore: update interview login base url

--- config/test-groups/interview_login.ts
+++ config/test-groups/interview_login.ts
@@ -1,7 +1,7 @@
 import type { GroupTestOptions } from '../../utils/groupContext';
 
 export const interviewLoginGroupConfig = {
-  baseURL: 'https://localhost:5173',
+  baseURL: 'https://localhost:5172',
   initialURL: '/#/pages/login/index',
   headers: undefined,
   cookies: []
